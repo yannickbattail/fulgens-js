cd93bbd6525bb2746d47dd21304527a838a80979
Yannick Battail <yannick.battail@gmail.com>
2018-10-31 17:44:53 +0100

    upgrade for 2 drones

--- timed-sequancial.js
+++ timed-sequancial.js
@@ -1,54 +1,94 @@
-
 function logResult(response) {
-	$('#out').html(response);
+    console.log(response);
 }
+var minePosition = {x:0,y:0};
+var factoryPosition = {x:0,y:0};
 
-function collect(droneName)
+ailurusApi.map(function(theMap){
+  console.log(theMap);
+  minePosition = theMap.mines[0].position;
+  factoryPosition = theMap.factories[0].position;
+  start();
+});
+
+function Collect(drone)
 {
-	var instruction = {
-		'TYPE': 'Collect',
-		'DroneName': droneName,
-		'Destination': null
-	};
-	ailurusApi.instructions([instruction], logResult);
+    var instruction = {
+        'TYPE': 'Collect',
+        'DroneName': drone,
+        'Destination': null
+    };
+    ailurusApi.instructions([instruction], logResult);
 }
-
-function unload(droneName)
+function MoveToMine(drone)
 {
-	var instruction = {
-		'TYPE': 'Unload',
-		'DroneName': droneName,
-		'Destination': null
-	};
-	ailurusApi.instructions([instruction], logResult);
+    var instruction = {
+        'TYPE': 'MoveTo',
+        'DroneName': drone,
+        'Destination': minePosition
+    };
+    ailurusApi.instructions([instruction], logResult);
 }
-
-function gotoMine(droneName)
+function MoveToFactory(drone)
 {
-	var instruction = {
-		'TYPE': 'MoveTo',
-		'DroneName': droneName,
-		'Destination': {X: 98, Y:98}
-	};
-	ailurusApi.instructions([instruction], logResult);
+    var instruction = {
+        'TYPE': 'MoveTo',
+        'DroneName': drone,
+        'Destination': factoryPosition
+    };
+    ailurusApi.instructions([instruction], logResult);
 }
-
-function gotoHome(droneName)
+function Unload(drone)
 {
-	var instruction = {
-		'TYPE': 'MoveTo',
-		'DroneName': droneName,
-		'Destination': {X: 2, Y:2}
-	};
-	ailurusApi.instructions([instruction], logResult);
+    var instruction = {
+        'TYPE': 'Unload',
+        'DroneName': drone,
+        'Destination': null
+    };
+    ailurusApi.instructions([instruction], logResult);
 }
-gotoMine('Drone_1');
-gotoMine('Drone_2');
-Fulgens.setTimeout(() => collect('Drone_1'), 136*1000);
-Fulgens.setTimeout(() => collect('Drone_2'), 136*1000);
-Fulgens.setTimeout(() => gotoHome('Drone_1'), 142*1000);
-Fulgens.setTimeout(() => gotoHome('Drone_2'), 142*1000);
-Fulgens.setTimeout(() => unload('Drone_1'), 280*1000);
-Fulgens.setTimeout(() => unload('Drone_2'), 280*1000);
 
-	  
+function drone1Collect(drone){
+    Collect('Drone_1');
+}
+function drone1MoveToMine(drone){
+    MoveToMine('Drone_1');
+}
+function drone1MoveToFactory(drone){
+    MoveToFactory('Drone_1');
+}
+function drone1Unload(drone){
+    Unload('Drone_1');
+}
+
+function drone2Collect(drone){
+    Collect('Drone_2');
+}
+function drone2MoveToMine(drone){
+    MoveToMine('Drone_2');
+}
+function drone2MoveToFactory(drone){
+    MoveToFactory('Drone_2');
+}
+function drone2Unload(drone){
+    Unload('Drone_2');
+}
+
+function start() {
+
+    drone1MoveToMine();
+    // in 2min drone1 will Collect
+    Fulgens.setTimeout(drone1Collect, 2*60*1000); // 2min in ms
+    // in 2min and 10s drone1 will Move To Factory
+    Fulgens.setTimeout(drone1MoveToFactory, 2*60*1000 + 10*1000);
+    // in 2min and 10s and 2min drone1 will Unload
+    Fulgens.setTimeout(drone1Unload, 2*60*1000 + 10*1000 + 2*60*1000);
+
+    drone2MoveToMine();
+    // in 2min drone2 will Collect
+    Fulgens.setTimeout(drone2Collect, 2*60*1000); // 2min in ms
+    // in 2min and 10s drone2 will Move To Factory
+    Fulgens.setTimeout(drone2MoveToFactory, 2*60*1000 + 10*1000);
+    // in 2min and 10s and 2min drone2 will Unload
+    Fulgens.setTimeout(drone2Unload, 2*60*1000 + 10*1000 + 2*60*1000);
+}
